3762cc7b1802d73f4bbf1d2c363be19ff3e2110c
Cedric Ait Sahel <ced.sahel@gmail.com>
2024-07-10 11:38:47 +0200

    add form register

--- app/src/main/java/com/example/senanas/views/MainActivity.kt
+++ app/src/main/java/com/example/senanas/views/MainActivity.kt
@@ -1,12 +1,26 @@
 package com.example.senanas.views
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
+import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
 import com.example.senanas.R
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var registerButton: Button
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        registerButton = findViewById(R.id.registerButton)
+
+        registerButton.setOnClickListener {
+            Intent(this, RegisterActivity::class.java).also {
+                startActivity(it)
+            }
+        }
+
     }
 }
--- app/src/main/java/com/example/senanas/views/RegisterActivity.kt
+++ app/src/main/java/com/example/senanas/views/RegisterActivity.kt
@@ -1,6 +1,8 @@
 package com.example.senanas.views
 
+import android.annotation.SuppressLint
 import android.os.Bundle
+import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -8,14 +10,11 @@ import androidx.core.view.WindowInsetsCompat
 import com.example.senanas.R
 
 class RegisterActivity : AppCompatActivity() {
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_register)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
+        
     }
 }
